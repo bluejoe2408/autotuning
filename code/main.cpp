1842ed3fb5f6d76814d828bf680c0c6e942a9f92
bbb <bluejoe2408@gmail.com>
2019-05-05 13:47:24 +0800

    fix bug

--- code/main.cpp
+++ code/main.cpp
@@ -15,6 +15,7 @@ int main(int argc, char **argv)
 {
     // host variable
     uint *h_SrcKey, *h_SrcVal, *h_DstKey, *h_DstVal;
+    uint *hb_SrcKey, *hb_SrcVal, *hb_DstKey, *hb_DstVal;
     uint *hm_SrcKey, *hm_SrcVal, *hm_BufKey, *hm_BufVal, *hm_DstKey, *hm_DstVal;
     uint *hq_SrcKey, *hq_SrcVal, *hq_BufKey, *hq_BufVal, *hq_DstKey, *hq_DstVal;
     // device variable
@@ -49,6 +50,10 @@ int main(int argc, char **argv)
     h_SrcVal = (uint *)malloc(tmpN * sizeof(uint));
     h_DstKey = (uint *)malloc(tmpN * sizeof(uint));
     h_DstVal = (uint *)malloc(tmpN * sizeof(uint));
+    hb_SrcKey = (uint *)malloc(tmpN * sizeof(uint));
+    hb_SrcVal = (uint *)malloc(tmpN * sizeof(uint));
+    hb_DstKey = (uint *)malloc(tmpN * sizeof(uint));
+    hb_DstVal = (uint *)malloc(tmpN * sizeof(uint));
 
     srand(2019);
 
@@ -57,13 +62,21 @@ int main(int argc, char **argv)
         h_SrcKey[i] = rand() % numValues;
     }
 
-
     /* pending add 0*/
     for(uint i = N; i < tmpN; i++ ) h_SrcKey[i] = 0;
     N = tmpN;
 
     fillValues(h_SrcVal, N);
 
+    for (uint i = 0; i < N; i++)
+    {
+        hb_SrcKey[i] = h_SrcKey[i];
+        hb_SrcVal[i] = h_SrcVal[i];
+    }
+
+
+    sdkResetTimer(&hTimer);
+    sdkStartTimer(&hTimer);
     printf("Allocating and initializing host merge sort arrays...\n\n");
     hm_SrcKey = (uint *)malloc(N * sizeof(uint));
     hm_SrcVal = (uint *)malloc(N * sizeof(uint));
@@ -73,11 +86,7 @@ int main(int argc, char **argv)
     hm_DstVal = (uint *)malloc(N * sizeof(uint));
     memcpy(hm_SrcKey, h_SrcKey, N * sizeof(uint));
     memcpy(hm_SrcVal, h_SrcVal, N * sizeof(uint));
-
-
     printf("Running CPU merge sort...\n");
-    sdkResetTimer(&hTimer);
-    sdkStartTimer(&hTimer);
     mergeSortHost(
         hm_DstKey,
         hm_DstVal,
@@ -109,6 +118,8 @@ int main(int argc, char **argv)
                       );
 
 
+    sdkResetTimer(&hTimer);
+    sdkStartTimer(&hTimer);
     printf("\nAllocating and initializing host quick sort arrays...\n\n");
     hq_SrcKey = (uint *)malloc(N * sizeof(uint));
     hq_SrcVal = (uint *)malloc(N * sizeof(uint));
@@ -120,12 +131,28 @@ int main(int argc, char **argv)
     memcpy(hq_SrcVal, h_SrcVal, N * sizeof(uint));
 
     printf("Running CPU quick sort...\n");
-    sdkResetTimer(&hTimer);
-    sdkStartTimer(&hTimer);
     std::sort(hq_SrcKey, hq_SrcKey + N);
     sdkStopTimer(&hTimer);
     printf("Time: %f ms\n", sdkGetTimerValue(&hTimer));
+    printf("Inspecting the results...\n");
+    keysFlag = validateSortedKeys(
+            hq_DstKey,
+            hq_SrcKey,
+            1,
+            N,
+            numValues,
+            DIR
+    );
+    valuesFlag = validateSortedValues(
+            hq_DstKey,
+            hq_DstVal,
+            hq_SrcKey,
+            1,
+            N
+    );
 
+    sdkResetTimer(&hTimer);
+    sdkStartTimer(&hTimer);
     printf("\nAllocating and initializing CUDA merge arrays...\n\n");
     checkCudaErrors(cudaMalloc((void **)&d_DstKey, N * sizeof(uint)));
     checkCudaErrors(cudaMalloc((void **)&d_DstVal, N * sizeof(uint)));
@@ -135,24 +162,10 @@ int main(int argc, char **argv)
     checkCudaErrors(cudaMalloc((void **)&d_SrcVal, N * sizeof(uint)));
     checkCudaErrors(cudaMemcpy(d_SrcKey, h_SrcKey, N * sizeof(uint), cudaMemcpyHostToDevice));
     checkCudaErrors(cudaMemcpy(d_SrcVal, h_SrcVal, N * sizeof(uint), cudaMemcpyHostToDevice));
-    printf("Allocating and initializing CUDA bitonic arrays...\n\n");
-    checkCudaErrors(cudaMalloc((void **)&db_DstKey, N * sizeof(uint)));
-    checkCudaErrors(cudaMalloc((void **)&db_DstVal, N * sizeof(uint)));
-    checkCudaErrors(cudaMalloc((void **)&db_BufKey, N * sizeof(uint)));
-    checkCudaErrors(cudaMalloc((void **)&db_BufVal, N * sizeof(uint)));
-    checkCudaErrors(cudaMalloc((void **)&db_SrcKey, N * sizeof(uint)));
-    checkCudaErrors(cudaMalloc((void **)&db_SrcVal, N * sizeof(uint)));
-    checkCudaErrors(cudaMemcpy(db_SrcKey, h_SrcKey, N * sizeof(uint), cudaMemcpyHostToDevice));
-    checkCudaErrors(cudaMemcpy(db_SrcVal, h_SrcVal, N * sizeof(uint), cudaMemcpyHostToDevice));
-
-
     printf("Initializing GPU merge sort...\n");
     initMergeSort();
-
     printf("Running GPU merge sort...\n");
     checkCudaErrors(cudaDeviceSynchronize());
-    sdkResetTimer(&hTimer);
-    sdkStartTimer(&hTimer);
     mergeSort(
         d_DstKey,
         d_DstVal,
@@ -164,13 +177,11 @@ int main(int argc, char **argv)
         DIR
     );
     checkCudaErrors(cudaDeviceSynchronize());
-    sdkStopTimer(&hTimer);
-    printf("Time: %f ms\n", sdkGetTimerValue(&hTimer));
-
     printf("Reading back GPU merge sort results...\n");
     checkCudaErrors(cudaMemcpy(h_DstKey, d_DstKey, N * sizeof(uint), cudaMemcpyDeviceToHost));
     checkCudaErrors(cudaMemcpy(h_DstVal, d_DstVal, N * sizeof(uint), cudaMemcpyDeviceToHost));
-
+    sdkStopTimer(&hTimer);
+    printf("Time: %f ms\n", sdkGetTimerValue(&hTimer));
     printf("Inspecting the results...\n");
     keysFlag = validateSortedKeys(
                         h_DstKey,
@@ -180,7 +191,6 @@ int main(int argc, char **argv)
                         numValues,
                         DIR
                     );
-
     valuesFlag = validateSortedValues(
                           h_DstKey,
                           h_DstVal,
@@ -188,17 +198,25 @@ int main(int argc, char **argv)
                           1,
                           N
                       );
-
     printf("Shutting down...\n");
     closeMergeSort();
 
-    printf("\nInitializing GPU bitonic sort...\n");
-    initMergeSort();
 
-    printf("Running GPU bitonic sort...\n");
-    checkCudaErrors(cudaDeviceSynchronize());
     sdkResetTimer(&hTimer);
     sdkStartTimer(&hTimer);
+    printf("Allocating and initializing CUDA bitonic arrays...\n\n");
+    checkCudaErrors(cudaMalloc((void **)&db_DstKey, N * sizeof(uint)));
+    checkCudaErrors(cudaMalloc((void **)&db_DstVal, N * sizeof(uint)));
+    checkCudaErrors(cudaMalloc((void **)&db_BufKey, N * sizeof(uint)));
+    checkCudaErrors(cudaMalloc((void **)&db_BufVal, N * sizeof(uint)));
+    checkCudaErrors(cudaMalloc((void **)&db_SrcKey, N * sizeof(uint)));
+    checkCudaErrors(cudaMalloc((void **)&db_SrcVal, N * sizeof(uint)));
+    checkCudaErrors(cudaMemcpy(db_SrcKey, hb_SrcKey, N * sizeof(uint), cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(db_SrcVal, hb_SrcVal, N * sizeof(uint), cudaMemcpyHostToDevice));
+    printf("\nInitializing GPU bitonic sort...\n");
+    initMergeSort();
+    printf("Running GPU bitonic sort...\n");
+    checkCudaErrors(cudaDeviceSynchronize());
     bitonicSort(
         db_DstKey,
         db_DstVal,
@@ -210,13 +228,11 @@ int main(int argc, char **argv)
         DIR
     );
     checkCudaErrors(cudaDeviceSynchronize());
-    sdkStopTimer(&hTimer);
-    printf("Time: %f ms\n", sdkGetTimerValue(&hTimer));
-
     printf("Reading back GPU bitonic sort results...\n");
     checkCudaErrors(cudaMemcpy(h_DstKey, d_DstKey, N * sizeof(uint), cudaMemcpyDeviceToHost));
     checkCudaErrors(cudaMemcpy(h_DstVal, d_DstVal, N * sizeof(uint), cudaMemcpyDeviceToHost));
-
+    sdkStopTimer(&hTimer);
+    printf("Time: %f ms\n", sdkGetTimerValue(&hTimer));
     printf("Inspecting the results...\n");
     keysFlag = validateSortedKeys(
                         h_DstKey,
@@ -234,7 +250,6 @@ int main(int argc, char **argv)
                           1,
                           N
                       );
-
     printf("Shutting down...\n");
     closeMergeSort();
 
@@ -245,10 +260,20 @@ int main(int argc, char **argv)
     checkCudaErrors(cudaFree(d_BufKey));
     checkCudaErrors(cudaFree(d_DstVal));
     checkCudaErrors(cudaFree(d_DstKey));
+    checkCudaErrors(cudaFree(db_SrcVal));
+    checkCudaErrors(cudaFree(db_SrcKey));
+    checkCudaErrors(cudaFree(db_BufVal));
+    checkCudaErrors(cudaFree(db_BufKey));
+    checkCudaErrors(cudaFree(db_DstVal));
+    checkCudaErrors(cudaFree(db_DstKey));
     free(h_DstVal);
     free(h_DstKey);
     free(h_SrcVal);
     free(h_SrcKey);
+    free(hb_DstVal);
+    free(hb_DstKey);
+    free(hb_SrcVal);
+    free(hb_SrcKey);
 
     exit((keysFlag && valuesFlag) ? EXIT_SUCCESS : EXIT_FAILURE);
 }
